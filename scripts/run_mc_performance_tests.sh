2db39cb9132e73a1357df150a47f2f13eb1bdaff
CN on VM <Charmaine.Ndolo@hu-berlin.de>
2022-04-01 09:07:29 +0000

    Scripts for performance tests

--- scripts/run_mc_performance_tests.sh
+++ scripts/run_mc_performance_tests.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+source $HOME/.bashrc
 
 BIN_DIR=$HOME/thesis/fbas_reward_distributor
+
 JOBS=8
 OUTPUT_FILE=results/ranking_algorithms_performance_measurements_mobilecoin_2022_03_29.csv
 FBAS_TYPE=mobile-coin
-ITERATIONS=2
+ITERATIONS=10
 MAX_TOP_TIER=35
+
+LOG_FILE=logs/mc_performance_tests_2022_03_29.log
 
 cd $BIN_DIR
 mkdir logs
+mkdir results
+
 cargo build --release --bin performance_tests --features "measurements"
-target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -j $JOBS $FBAS_TYPE >& logs/mc_performance_tests_2022_03_29.log
+#target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -j $JOBS -u $FBAS_TYPE
+target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -j $JOBS -u $FBAS_TYPE > $LOG_FILE 2>&1
--- scripts/run_stellar_performance_tests.sh
+++ scripts/run_stellar_performance_tests.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+source $HOME/.bashrc
 
-#BIN_DIR=$HOME/thesis/fbas_reward_distributor
-BIN_DIR=fbas_reward_distributor
-JOBS=4
+BIN_DIR=$HOME/thesis/fbas_reward_distributor
+
+JOBS=8
 OUTPUT_FILE=results/ranking_algorithms_performance_measurements_stellar_2022_03_29.csv
 FBAS_TYPE=stellar
-ITERATIONS=1
+ITERATIONS=10
 MAX_TOP_TIER=35
+
+LOG_FILE=logs/stellar_performance_tests_2022_03_29.log
 
 cd $BIN_DIR
 mkdir logs
+mkdir results
+
 cargo build --release --bin performance_tests --features "measurements"
-target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -j $JOBS -u $FBAS_TYPE >& logs/stellar_performance_tests_2022_03_29.log
-#target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -u -j $JOBS $FBAS_TYPE
+#target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -j $JOBS -u $FBAS_TYPE
+target/release/performance_tests -m $MAX_TOP_TIER  -r $ITERATIONS -o $OUTPUT_FILE -j $JOBS -u $FBAS_TYPE > $LOG_FILE 2>&1
