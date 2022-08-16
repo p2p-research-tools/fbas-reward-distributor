502262353b8ac838715b65ff98eb4c7e8dd70e01
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2022-08-16 11:43:42 +0200

    Add some unit tests to features and run perf tests regardless of n

--- tests/batch_cli.rs
+++ tests/batch_cli.rs
@@ -2,12 +2,15 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[test]
-#[ignore]
-// ignore for now because binary only available when feature is active
-// causes coverage test to fail
-fn valid_batch_perf_experiments_command() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("batch_performance_eval")?;
-    cmd.arg("-r").arg("1").arg("-m").arg("1").arg("stellar");
+#[cfg_attr(not(feature = "measurements"), ignore)]
+fn valid_batch_perf_experiments() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("performance_tests")?;
+    cmd.arg("-r")
+        .arg("1")
+        .arg("-m")
+        .arg("1")
+        .arg("stellar")
+        .arg("power-index-enum");
     cmd.assert().success().stdout(predicate::str::contains(
         "Starting performance measurements for Stellar like FBAS with upto 1 nodes.\n Performing 1 iterations per FBAS.",
     ));
@@ -15,12 +18,10 @@ fn valid_batch_perf_experiments_command() -> Result<(), Box<dyn std::error::Erro
 }
 
 #[test]
-#[ignore]
+#[cfg_attr(not(feature = "measurements"), ignore)]
 fn no_fbas_type_in_command() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("batch_error_eval")?;
-    cmd.arg("--features")
-        .arg("measurements")
-        .arg("-m")
+    let mut cmd = Command::cargo_bin("approximation_tests")?;
+    cmd.arg("-m")
         .arg("10")
         .arg("-j")
         .arg("4")
